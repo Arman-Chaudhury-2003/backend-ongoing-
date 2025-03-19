8c23a203aa611996ea142252f5be4cbb397a7804
Arman-Chaudhury-2003 <ac7707@srmist.edu.in>
2025-03-20 00:10:50 +0530

    setup done

--- src/app.js
+++ src/app.js
@@ -16,4 +16,11 @@ app.use(express.json({ limit: "16kb" })); //json theke jokhon data asbe(like for
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //url theke jokhon data asbe.
 app.use(express.static("public")); //images and all
 app.use(cookieParser()); //CRUD options on cookies given by users
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
+
 export { app };
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,19 +1,25 @@
 // const asyncHandler = () => {};
 
+const asyncHandler = (requestHandler) => {
+  return (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
 export { asyncHandler };
 
 //wrapper to not repeat db index.js again
 //(req, res) into (err,req,res,next)
 //const something=()=>()=>{}
-const asyncHandler = (fn) => async (req, res, next) => {
-  try {
-    await fn(req, res, next);
-  } catch (err) {
-    res.status(err.code || 500).json({
-      success: false,
-      message: err.message,
-    });
-    //when err is we send either err code if by user or 500 if by server
-    //we also send a json formated respose for err message
-  }
-};
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (err) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//     //when err is we send either err code if by user or 500 if by server
+//we also send a json formated respose for err message
+//   }
+// };
